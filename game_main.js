9ee2e238507f3cb6a94f9445721f5545c13ecf8a
Cryodine <44298573+Cryodine@users.noreply.github.com>
2019-01-18 08:56:05 -0800

    Update game_main.js

--- game_main.js
+++ game_main.js
@@ -1,6 +1,11 @@
 function start(){
   
   console.log("hello world");
-  alert("sombody one told me");
+  alert("This is a test of the alert system");
+  
+  var canvas = document.getElementById('test');
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'green';
+  ctx.fillRect(10, 10, 100, 100);
 
 }
