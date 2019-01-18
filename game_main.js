dc614c459b97d7287e0eb1595bf6abc157a7f559
Cryodine <44298573+Cryodine@users.noreply.github.com>
2019-01-18 13:41:18 -0800

    Update game_main.js

--- game_main.js
+++ game_main.js
@@ -7,5 +7,14 @@ function start(){
   var ctx = canvas.getContext('2d',{ alpha: false });
   ctx.fillStyle = 'green';
   ctx.fillRect(10, 10, 100, 100);
+  
+  document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 37) {
+        alert('Left was pressed');
+    }
+    else if(event.keyCode == 39) {
+        alert('Right was pressed');
+    }
+  });
 
 }
