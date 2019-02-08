f8484056a1831b5868eaaaaa8bb59b17811284c7
Cryodine <44298573+Cryodine@users.noreply.github.com>
2019-02-08 08:35:07 -0800

    Update game_main.js

--- game_main.js
+++ game_main.js
@@ -3,10 +3,22 @@ function start(){
   console.log("hello world");
   alert("This is a test of webgl");
   
+  var test = 100;
+  
   var canvas = document.getElementById('test');
   var ctx = canvas.getContext('2d',{ alpha: false });
   ctx.fillStyle = 'green';
-  ctx.fillRect(10, 10, 100, 100);
+  ctx.fillRect(10, 10, 100, test);
+  
+  function test(a){
+    
+    if(a.keyCode == 13){
+      test = 200;
+    }
+    
+  }
+  
+  document.addEventListener('keydown', test(event));
   
   document.addEventListener('keydown', function(event) {
     if(event.keyCode == 37) {
